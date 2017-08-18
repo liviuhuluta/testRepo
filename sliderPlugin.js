b05c88170e8fc5224949470e10c845514acef2fc
Liviu <liviuhuluta3@gmail.com>
2017-08-18 17:17:40 +0300

    added sliderPlugin.js

---
+++ sliderPlugin.js
@@ -0,0 +1,115 @@
+(function ( $ ) {
+ 
+    $.fn.makeSlider = function( options ) {
+		var slider=this;
+		var elems,currentPage=1;
+        var settings = $.extend({
+            // These are the defaults.
+            animate:true,
+            transitionTime: "500",
+			vertical:false,
+			displayPrevNext:true,
+			infiniteTransition:true,
+			slideEl:'li',
+        }, options );
+		
+		this.css({
+			backgroundColor: "gray",
+			width          : "60vw",
+			height         : "20vh",
+			left           : "20vw",
+			position       : "relative",
+			top            : "1vw",
+			overflow       : "hidden",
+			display        : "inline-block",
+			"word-wrap"    : "break-word",
+			"white-space"  : "nowrap",
+		});
+		
+		
+		this.find( settings.slideEl ).css({
+			backgroundColor: "beige",
+			width          : "40vw",
+			height         : "18vh",
+			marginLeft     : "2px",
+			left           : "-30vw",
+			position       : "relative",
+			display        : "inline-block",
+			textAlign      : "center",
+			lineHeight     : "990%",
+			top            : "0.7vw",
+		});
+		function makeButtons(){
+			
+			var buttonNo=slider.children().length;
+			var parentClass=slider.attr('class');
+			$('<span />',{ class: parentClass+'button' }).appendTo(slider);
+			
+			for(var i=1;i<=buttonNo-2;i++){
+				$('<button />',{ class: 'button', id:+i}).click(function(){callPage($(this).attr('id')-1);}).appendTo($('.'+parentClass+'button'));
+			}
+			
+			
+			
+			$('.'+parentClass+'button').css({
+				width              : "20vw",
+				position           : "absolute",
+				display            : "inline-block",
+				top                : "16vh",
+				left               : "25vw",
+				
+			});
+			
+			$('.button').css({
+				backgroundColor    : "olive",
+				width              : "1.3vw",
+				height             : "1.3vw",
+				marginLeft         : "10px",
+				"border-radius"    : "100%",
+				display            : "inline-block",
+				margin             : "auto",
+
+			});
+		}
+		makeButtons();
+		
+		function createArray(){
+			elems=[];
+			var first=$('this:last-child');
+			elems.push(first);
+			for (var i=1;i<=slider.children().length-1;i++){
+				elems.push($('slider >div:nth-child('+i+')'));
+			}
+			var last=$('this:first-child');
+			elems.push(last);
+		}
+		createArray();
+		
+		function callPage(x){
+			currentPage=x;
+			distance=-30-40*x+"vw";
+			slider.find( settings.slideEl ).animate({left : distance});
+		}
+		
+    };
+ 
+}( jQuery ));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ test.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+	$('.slider').makeSlider({
+		slideEl:'div'
+	});
+	
+	$('.slider2').makeSlider({
+		slideEl:'div'
+	});
+	
+	
+	$('#slider3').makeSlider({
+		slideEl:'img'
+	});
+	
+});
